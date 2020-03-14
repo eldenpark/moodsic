cd87b8e56193c184837cc81f10e006ee34bc8a98
Elden <eldeniyenden@gmail.com>
2020-03-14 01:55:41 -0700

    docs: fix favicon reference

--- packages/moodsic-web/src/server/makeHtml.prod.tsx
+++ packages/moodsic-web/src/server/makeHtml.prod.tsx
@@ -62,8 +62,11 @@ function template({
   return `
 <html>
   <head>
+    <title>Moodsic: Mood classify sound</title>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0 name="viewport">
+    <link rel="icon" type="image/x-icon" href="/moodsic/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/moodsic/favicon.ico" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap" rel="stylesheet">
     <style>${fontAwesomeCss}</style>
--- packages/moodsic-web/src/server/makeHtml.tsx
+++ packages/moodsic-web/src/server/makeHtml.tsx
@@ -69,8 +69,10 @@ function template({
   return `
 <html>
   <head>
+    <title>Moodsic: Mood classify sound</title>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0 name="viewport">
+    <link rel="icon" href="/moodsic/favicon.png">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap" rel="stylesheet">
     <style>${fontAwesomeCss}</style>
